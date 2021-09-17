613fc3953b5925944dbcdf2de784d953f7a88399
milosz.zajonc <milosz.zajonc@proficredit.pl>
2021-09-17 10:15:31 +0200

    test commit

--- src/views/Root/Root.tsx
+++ src/views/Root/Root.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+const unusedVar = '';
+
 const Root = () => {
-  return <div className="App">hello</div>;
+  return <div className="App">learn react</div>;
 };
 
 export default Root;
